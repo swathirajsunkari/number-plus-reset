8456a64ee5946a7f5adc0dc2e57b1ab656c077dd
Swathi <pinky.nani11@gmail.com>
2023-11-11 23:51:18 +0100

    Initial Commit

---
+++ number plus reset/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  number plus reset
+//
+//  Created by admin on 11/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ number plus reset/number_plus_resetApp.swift
@@ -0,0 +1,17 @@
+//
+//  number_plus_resetApp.swift
+//  number plus reset
+//
+//  Created by admin on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct number_plus_resetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
